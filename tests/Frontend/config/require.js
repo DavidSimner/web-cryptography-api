d0d35fc0dc7451ab4ad21abbd4ad5dc40ee43491
David Simner <david.simner@red-gate.com>
2015-01-18 15:42:22 +0000

    add hello world test

---
+++ tests/Frontend/config/require.js
@@ -0,0 +1,14 @@
+﻿var require = {
+    paths: {
+        sut: '../..',
+
+        asn1: 'Scripts/asn1',
+        common: 'Scripts/common',
+    },
+    shim: {
+        asn1: {
+            deps: ['common'],
+            exports: 'org.pkijs.asn1'
+        }
+    }
+};
---
+++ tests/Frontend/tests/cms.test.js
@@ -0,0 +1,11 @@
+﻿define(['sut/cms'], function (Cms) {
+    describe('Cms', function () {
+        describe('encrypt', function () {
+            describe('interoperability with BouncyCastle.Crypto', function () {
+                it('Hello World', function () {
+                    var sut = new Cms();
+                });
+            });
+        });
+    });
+});
