e7bb14117d89f36d5be3a959e35d7b51ff5c9e91
David Simner <david.simner@red-gate.com>
2015-01-18 21:08:24 +0000

    new style of writing the test

--- tests/Frontend/tests/cms.test.js
+++ tests/Frontend/tests/cms.test.js
@@ -5,10 +5,16 @@
         describe('encrypt', function () {
             describe('interoperability with BouncyCastle.Crypto', function () {
                 function doTest (value, done) {
-                    new Ajax('http://localhost:64189/api/keyGenerator').get().then(function (response) {
-                        expect(new Cms().encrypt(response.PublicKey, value)).to.be.fulfilled.and.notify(done);
+                    var arrange = new Ajax('http://localhost:64189/api/keyGenerator').get();
+
+                    var act = arrange.then(function (arrangement) {
+                        return new Cms().encrypt(arrangement.PublicKey, value);
                     });
-                };
+
+                    var assert = Promise.all([ arrange, act ]);
+
+                    expect(assert).to.be.fulfilled.and.notify(done);
+                }
 
                 it('Hello World', function (done) {
                     doTest('Hello World', done);
